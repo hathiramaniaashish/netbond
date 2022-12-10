1dbc0cd0394bf175517c5a13cd23641a65cad498
Aashish Hathiramani <hathiramaniaashish@gmail.com>
2022-12-10 21:36:40 +0000

    Pending requests list

--- app/src/main/java/com/example/netbond/MainActivity.kt
+++ app/src/main/java/com/example/netbond/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.netbond
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.navigation.findNavController
 import com.example.netbond.databinding.ActivityMainBinding
@@ -14,8 +15,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         window.statusBarColor = getColor(R.color.background)
         binding = ActivityMainBinding.inflate(layoutInflater)
-//        binding.toolBar.visibility = View.GONE
-//        binding.bottomNav.visibility = View.GONE
+        // binding.toolBar.visibility = View.GONE
+        // binding.bottomNav.visibility = View.GONE
         binding.root.setOnClickListener { hideKeyboard() }
         setUpBottomNav()
         setContentView(binding.root)
--- app/src/main/java/com/example/netbond/UserSearchBarFragment.kt
+++ app/src/main/java/com/example/netbond/UserSearchBarFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.example.netbond.databinding.AcceptUserTemplateBinding
 import com.example.netbond.databinding.FragmentUserSearchBarBinding
 import com.example.netbond.databinding.SearchUserTemplateBinding
 import com.google.firebase.firestore.FirebaseFirestore
@@ -18,6 +19,7 @@ class UserSearchBarFragment : Fragment() {
     private lateinit var binding: FragmentUserSearchBarBinding
     private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
     private var usersRef = db.collection("users")
+    private var actualUsername = "johndoe"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +28,7 @@ class UserSearchBarFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentUserSearchBarBinding.inflate(inflater, container, false)
         setSearchBarListener()
+        setReceivedRequests(actualUsername)
         return binding.root
     }
 
@@ -42,28 +45,55 @@ class UserSearchBarFragment : Fragment() {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 binding.usersList.removeAllViews()
                 if (!s.isNullOrEmpty()) {
-                    usersRef.get().addOnSuccessListener { users ->
-                        users.forEach { user ->
-                            val name = user.get("name").toString()
-                            if (name.contains(s)) {
-                                val bind = SearchUserTemplateBinding.inflate(layoutInflater, binding.usersList, true)
-                                bind.nameUser.text = name
-                                bind.userPoints.text = user.get("n_points").toString()
-                                val username = "@" + user.get("username").toString()
-                                bind.userName.text = username
-                                bind.user.tag = username
-                                bind.root.setOnClickListener { view ->
-                                    view.setBackgroundColor(resources.getColor(R.color.gray, null))
-                                    findNavController().navigate(R.id.feedFragment)
-                                }
-                            }
-                        }
+                    binding.pendingRequests.visibility = View.GONE
+                    setSearchUsers(s)
+                } else {
+                    binding.pendingRequests.visibility = View.VISIBLE
+                    setReceivedRequests(actualUsername)
+                }
+            }
+        })
+    }
+
+    private fun setSearchUsers(searchedText: CharSequence) {
+        usersRef.get().addOnSuccessListener { users ->
+            users.forEach { user ->
+                val name = user.get("name").toString()
+                if (name.contains(searchedText)) {
+                    val bind = SearchUserTemplateBinding.inflate(layoutInflater, binding.usersList, true)
+                    bind.nameUser.text = name
+                    bind.userPoints.text = user.get("n_points").toString()
+                    val username = "@" + user.get("username").toString()
+                    bind.userName.text = username
+                    // bind.user.tag = username
+                    bind.root.setOnClickListener { view ->
+                        view.setBackgroundColor(resources.getColor(R.color.gray, null))
+                        findNavController().navigate(R.id.feedFragment)
                     }
                 }
             }
+        }
+    }
 
-        })
-
+    private fun setReceivedRequests(username: String) {
+        usersRef.whereEqualTo("username", username).get().addOnSuccessListener {
+            val actualUserDoc = it.single()
+            usersRef
+                .document(actualUserDoc.id)
+                .collection("receivedRequests")
+                .get().addOnSuccessListener { requests ->
+                    requests.forEach { request ->
+                        usersRef.whereEqualTo("username", request.id).get().addOnSuccessListener {
+                            val userDoc = it.single()
+                            val bind = AcceptUserTemplateBinding.inflate(layoutInflater, binding.usersList, true)
+                            bind.nameUser.text = userDoc.get("name").toString()
+                            val username = "@" + userDoc.get("username").toString()
+                            bind.userName.text = username
+                            bind.userPoints.text = userDoc.get("n_points").toString()
+                        }
+                    }
+                }
+        }
     }
 
 }
