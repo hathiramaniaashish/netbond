2676246574fe2bc967b557c8f718c288decf18d5
Aashish Hathiramani <hathiramaniaashish@gmail.com>
2022-12-19 23:46:46 +0000

    Adding style to the Feed

--- app/src/main/java/com/example/netbond/SignUpActivity.kt
+++ app/src/main/java/com/example/netbond/SignUpActivity.kt
@@ -22,6 +22,7 @@ class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.statusBarColor = getColor(R.color.background)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         binding.root.setOnClickListener { hideKeyboard() }
         binding.signUpMessage2.setOnClickListener { navigateToActivity(LoginActivity()) }
